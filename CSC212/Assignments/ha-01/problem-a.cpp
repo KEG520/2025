1bf84b77f2a7d42d946ac6c75e6ac5414cd8eaae
KEZE <grantk5205@gmail.com>
2025-02-03 12:55:39 -0500

    Merge branch 'main' of https://github.com/KEG52/2025

---
+++ CSC212/Assignments/ha-01/problem-a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+main(){
+
+    std::string phrase;
+    std::cin >> phrase;
+
+    int i = 0;
+    while(i<=3){
+        std::cout << phrase << " ";
+
+    i++;
+    }
+
+    return 0;
+
+}
