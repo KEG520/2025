be05886f04dafae33b8f2f6e4608fe88a4c9e422
KEZE <grantk5205@gmail.com>
2025-02-04 15:38:08 -0500

    Create ethonal.cpp

---
+++ CSC212/Assignments/ha-01/ethonal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    #carbon atoms input
+    int c;
+    
+    while(!(std::cin >> c) || c >= || c <= 10){
+        std::cin.close();
+        std::cin.ignore(1000, '\n')
+    }
+    
+}
