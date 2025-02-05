78a2c834f25047dfb471141ddd9b8a485bb7c8f8
KEZE <grantk5205@gmail.com>
2025-02-05 05:18:33 -0500

    2/5/205

--- CSC212/Assignments/ha-01/problem-d.cpp
+++ CSC212/Assignments/ha-01/problem-d.cpp
@@ -6,15 +6,44 @@ int main(){
     //CPR Number 
 
     // 10 digit number (6 repesent the birthday) (4 are a sequence number)
+    //example number 070761-4285
 
     std::string cpr;
+    std::cin >> cpr;
 
-    // get rid of the dash
+    // get rid of the dash // Complete
+    //source https://www.geeksforgeeks.org/stdstringerase-in-cpp/
+
+    cpr.erase(cpr.begin() + 6);
+
+    // std::cout << cpr;
+
+
+
+    // append each number into a vector 0-9 (10 digits) 
+    std::vector<char> numbersstr;
+
+    for (int i=0; i < cpr.length(); i++){
+        numbersstr.push_back(cpr[i]);;
+
+    }
+    for (int i = 0; i < numbersstr.size(); i++) {
+        std::cout << numbersstr[i] << " ";
+    }
+
     // turn each char into a readable interger  (int)
-    // append each number into a vector 0-9 (10 digits)
+    // using the stoi method (string to int)
+    //sourcce https://www.geeksforgeeks.org/convert-string-to-int-in-cpp/?ref=header_outind
+    
+
+
+
+
     // a based vector that included the cpr sequence
     //
 
+
+    return 0;
 
 
 } 
