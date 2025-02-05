5b000f0561d5b614fd87d0f864a2213b8cf174c3
KEZE <grantk5205@gmail.com>
2025-02-05 04:05:24 -0500

    pro

---
+++ CSC212/Assignments/ha-01/problem-d.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+int main(){
+
+    //CPR Number 
+
+    // 10 digit number (6 repesent the birthday) (4 are a sequence number)
+
+    std::string cpr;
+
+    // get rid of the dash
+    // turn each char into a readable interger  (int)
+    // append each number into a vector 0-9 (10 digits)
+    // a based vector that included the cpr sequence
+    //
+
+
+
+} 
