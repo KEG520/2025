0945bca9396b6be762901a62ec1142ec7ee73e5d
KEZE <grantk5205@gmail.com>
2025-03-05 16:50:33 -0500

    homework

---
+++ CSC212/Assignments/asn3/server.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main(){
+
+int n;
+int t;
+
+std::cin >> n;
+
+std::cin >> t;
+
+
+int arr[n];
+
+for (int i = 0; i < n; i++){
+    std::cin >> arr[i];
+}
+
+int total = 0;
+int completed = 0;
+
+for (int i = 0; i < n; i++){
+    if (total + arr[i] <= t){
+        total += arr[i];
+        completed++;
+    }
+    else{
+        break;
+    }
+   
+}
+std::cout << completed;
+}
