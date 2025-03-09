4d42bf2db0061a6e88ab79779d0796df47e8ec19
KEZE <grantk5205@gmail.com>
2025-03-09 15:19:06 -0400

    fixed

---
+++ Swiss_Cheese/Game.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+int main(){
+
+    std::cout << "Welcome to SWISS_CHEESE" << "\n" << "A GODLYGEEKS GAME" << '\n' << "-KADDEN GRANT-" << '\n';
+
+    // start game
+
+    std::cout << "Start Game" << "\n" << " (Y/N)";
+
+    std::string start;
+    std::cin >> start;
+
+    for (int i = 0; i < 0; i++){
+        if (start == 'N' || start == 'n'){
+            std::cout << "YOU ARE NOT READY FOR A CHALLENGE" << " " << '\n' << "LATER FN";
+        }
+        else if (start)
+    }
+}
