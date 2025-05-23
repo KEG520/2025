d90f7f77d0b48e4344235942f66b45799f9670b5
KEZE <121184402+KEG520@users.noreply.github.com>
2025-05-23 13:27:31 -0400

    updated

--- PYPRACT/test.py
+++ PYPRACT/test.py
@@ -1,3 +1,48 @@
 
+# first = "lol"
 
-n = int()
+# last = "ok"
+
+# full_text = f"{first} {last}"
+# print(full_text)
+
+
+# x,y,z = 20,90,4
+
+# print(x)
+# print(y)
+# print(z)
+
+# lists
+
+# bikes = ['dog', 'cat', 'frog', 'poop']
+# print(bikes[-1].title())
+
+# if len(bikes) < 2:
+#     bikes.append('tree')
+#     print(bikes[-1])
+# else:
+#     print("nah")
+
+# cars = ['bmw', 'audi', 'toyota', 'subaru']
+# cars.reverse()
+# print(len(cars))
+
+# print(cars)
+# print(sorted(cars))
+# cars.sort(reverse=True)
+# print(cars)
+
+# Heros = ['ironman', 'hulk', 'widow']
+
+# for hero in Heros:
+#     print(f"{hero.title()}")
+
+# for valu in range(1,10):
+#     print(valu)
+
+
+# Heros = ['ironman', 'hulk', 'widow']
+
+# print(Heros[1:3])
+
